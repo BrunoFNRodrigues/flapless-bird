676e6b9f269fe965829a9685f0938139d0a41cf9
NicolasCho <nicolasbkc@gmail.com>
2023-05-12 14:04:45 -0300

    test scene

---
+++ FlaplessBird/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Touch touch;
+    private Vector2 touchStartPosition, touchEndPosition;
+    private string direction;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
