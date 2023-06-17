8786da6bd5bd05df073744b15497176f2b16dcb7
Rodrigo Nigri Griner <rodrigong1@al.insper.edu.br>
2023-06-17 00:46:26 -0400

    o background desacelerou, mas os obstaculos ainda nao

--- FlaplessBird/Assets/Scripts/MainGame/BackgroundLoop.cs
+++ FlaplessBird/Assets/Scripts/MainGame/BackgroundLoop.cs
@@ -9,12 +9,22 @@ public class BackgroundLoop : MonoBehaviour
     public bool stop = false;
     public GameObject manager;
 
+    public StoreMgmt storeMgmt;
+
     void Start(){
         manager = GameObject.Find("GameManager");
     }
 
     void Update(){
-        if(!manager.GetComponent<Manager>().stopSign)
-            backgroundRenderer.material.mainTextureOffset += new Vector2(0f, -backgroundSpeed * Time.deltaTime);
+        if(!manager.GetComponent<Manager>().stopSign){
+            if(storeMgmt.getUmbrella() == 1){
+                backgroundRenderer.material.mainTextureOffset += new Vector2(0f, -backgroundSpeed * Time.deltaTime * 0.5f);
+            }
+            else{
+                backgroundRenderer.material.mainTextureOffset += new Vector2(0f, -backgroundSpeed * Time.deltaTime);
+            }
+        }
+            
+            
     }
 }
